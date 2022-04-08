150a41f125975bfa3883c0313b392e15fe99b348
redoffma <redoffman@gamil.com>
2022-04-08 23:37:38 +0900

    칵테일 제조

---
+++ baekjoon/p_2896.py
@@ -0,0 +1,13 @@
+
+orange_all, apple_all, pine_all = map( int, input().split())
+orange_rate, apple_rate, pine_rate = map( int, input().split())
+
+min_rate = min( (orange_all/orange_rate) , apple_all/apple_rate, pine_all/pine_rate )
+
+
+print(  orange_all-(orange_rate*min_rate), apple_all-(apple_rate*min_rate), \
+            pine_all-(pine_rate*min_rate) )
+
+
+
+
