58fd722732e84fcd286556d31b213a7f5ac0b05a
redoffma <redoffman@gamil.com>
2022-04-10 02:32:20 +0900

    update

--- baekjoon/p_2896.py
+++ baekjoon/p_2896.py
@@ -4,7 +4,6 @@ orange_rate, apple_rate, pine_rate = map( int, input().split())
 
 min_rate = min( (orange_all/orange_rate) , apple_all/apple_rate, pine_all/pine_rate )
 
-
 print(  orange_all-(orange_rate*min_rate), apple_all-(apple_rate*min_rate), \
             pine_all-(pine_rate*min_rate) )
 
