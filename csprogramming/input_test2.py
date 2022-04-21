e1d31413071362ef895ca020a6faa9872ced8c63
offman <maguxoffman@gmail.com>
2022-04-21 13:49:36 +0900

    add input_test2.py

--- csprogramming/input_test2.py
+++ csprogramming/input_test2.py
@@ -3,8 +3,8 @@ import sys
 
 print( " Press Ctrl+d for exit" );
 input_list=[]
-for s in sys.stdin:
-  s=s.strip("\n")
+for _ in range(10):
+  s=input()
   print(f"s=[{s}]")
   input_list.append(s)
 
