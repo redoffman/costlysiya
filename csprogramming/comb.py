337af8830e22caa8b73fec219fb965b929661861
redoffma <redoffman@gamil.com>
2022-05-24 00:58:45 +0900

    order..

--- csprogramming/comb.py
+++ csprogramming/comb.py
@@ -2,17 +2,18 @@ result_list=[]
 def C(cnt, n , lt=[]):
     global result_list
     if n == 0:
-        result_list.append(sorted(lt))
+        result_list.append(lt)
         return
     elif cnt == 0:
         return
     else:
         lt2=lt.copy()
-        C(cnt-1, n,lt)
         lt2.append(cnt) 
         C(cnt-1, n-1,lt2)
+        C(cnt-1, n,lt)
+        
     
 
-C(10,2 )
+C(10,5 )
 print( len(result_list), result_list )
 
